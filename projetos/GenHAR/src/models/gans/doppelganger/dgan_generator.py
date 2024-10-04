fc301bcac29f8fd563ae14714f46ccaf42f8c0ba
Amparo Diaz Muñoz <amparo.munoz.eldorado@gmail.com>
2024-10-04 15:08:48 -0300

    dcgan reshape

--- projetos/GenHAR/src/models/gans/doppelganger/dgan_generator.py
+++ projetos/GenHAR/src/models/gans/doppelganger/dgan_generator.py
@@ -10,23 +10,24 @@ class DCGANGenerator:
 
     def train(self, X_train, y_train):
         # Adiciona a coluna de labels ao dataframe
-        df = X_train.copy()
-        df["label"] = y_train
+        #df = X_train.copy()
+        #df["label"] = y_train
+        reshape=True
+        if reshape:
+            n_amostras = X_train.shape[0]
+            X_train = X_train.values.reshape(n_amostras, 60, 6)
+            attributes = y_train.values.reshape(n_amostras, 1)
         
         # Configura o modelo DGAN
         self.model = DGAN(DGANConfig(
-            max_sequence_len=X_train.shape[1],
-            sample_len=self.config['parameters']['sample_len'],
+            max_sequence_len=60,
+            sample_len=6,
             batch_size=self.config['parameters']['batch_size'],
-            epochs=self.config['parameters']['epochs']
+            epochs=self.config['parameters']['epochs'],
         ))
 
         # Treina o modelo
-        self.model.train_dataframe(
-            df,
-            attribute_columns=["label"],
-            discrete_columns=["label"],
-        )
+        self.model.train_numpy(attributes=attributes, features=X_train)
         print("DGAN model training complete.")
 
     def generate(self, n_samples):
@@ -34,8 +35,15 @@ class DCGANGenerator:
             raise RuntimeError("The model has not been trained yet.")
         
         # Gera dados sintéticos
-        synthetic_df = self.model.generate_dataframe(n_samples)
-        print(synthetic_df.columns)
+        synthetic_attributes, synthetic_features  = self.model.generate_numpy(n_samples)
+        synthetic_features_flat = synthetic_features.reshape(n_samples, 60 * 6)
+    
+        # Criar DataFrame similar ao original X_train
+        synthetic_df = pd.DataFrame(synthetic_features_flat, columns=[f'feature_{i}' for i in range(360)])
+        
+        # Adiciona os atributos (rótulos) como uma coluna separada
+        synthetic_df['label'] = synthetic_attributes.flatten()
+            
         print(f"{n_samples} synthetic samples generated.")
         return synthetic_df
 
