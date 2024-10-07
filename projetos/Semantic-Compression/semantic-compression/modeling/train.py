00e9e35eb763a80f406983a2f6b1d86e6d00b67f
dioph <dioph@protonmail.com>
2024-10-06 23:05:50 -0300

    model_fname & save losses

--- projetos/Semantic-Compression/semantic-compression/modeling/train.py
+++ projetos/Semantic-Compression/semantic-compression/modeling/train.py
@@ -4,9 +4,10 @@ from tqdm.auto import tqdm
 from gan import GenerativeCompressionGAN
 from keras.utils import image_dataset_from_directory
 from keras import optimizers, losses
+import numpy as np
 
 
-def train(gan, dataloader_train, dataloader_val, epochs, save_every):
+def train(gan, dataloader_train, dataloader_val, epochs, save_every, model_fname):
     d_losses_train = []
     g_losses_train = []
     d_losses_val = []
@@ -20,7 +21,7 @@ def train(gan, dataloader_train, dataloader_val, epochs, save_every):
         d_losses_val.append(vdl)
         g_losses_val.append(vgl)
         if epoch % save_every == 0:
-            gan.save_weights(f'../../models/gan_{epoch}.weights.h5')
+            gan.save_weights(f'../models/{model_fname}_{epoch}.weights.h5')
         pbar.set_description(f'train: ({tdl:.2f}; {tgl:.2f}); val: ({vdl:.2f}; {vgl:.2f})')
     return d_losses_train, g_losses_train, d_losses_val, g_losses_val
 
@@ -28,17 +29,17 @@ def train(gan, dataloader_train, dataloader_val, epochs, save_every):
 def main():
     parser = argparse.ArgumentParser(description="Treinamento de Generative Compression GAN")
 
-    parser.add_argument('--path_train', type=str, default='../../data/train', help='Caminho para o dataset de treino')
-    parser.add_argument('--path_val', type=str, default='../../data/test', help='Caminho para o dataset de validação')
-    parser.add_argument('--H', type=int, default=128, help='Altura das imagens')
-    parser.add_argument('--W', type=int, default=256, help='Largura das imagens')
-    parser.add_argument('--width', type=int, default=32, help='Número de filtros de largura do GAN')
-    parser.add_argument('--depth', type=int, default=3, help='Número de blocos de profundidade do GAN')
-    parser.add_argument('--batch_size', type=int, default=50, help='Tamanho do batch para treinamento')
+    parser.add_argument('--path_train', type=str, default='../data/train', help='Caminho para o dataset de treino')
+    parser.add_argument('--path_val', type=str, default='../data/test', help='Caminho para o dataset de validação')
+    parser.add_argument('--H', type=int, default=256, help='Altura das imagens')
+    parser.add_argument('--W', type=int, default=512, help='Largura das imagens')
+    parser.add_argument('--width', type=int, default=128, help='Número de filtros de largura do GAN')
+    parser.add_argument('--depth', type=int, default=5, help='Número de blocos de profundidade do GAN')
+    parser.add_argument('--batch_size', type=int, default=32, help='Tamanho do batch para treinamento')
     parser.add_argument('--d_lr', type=float, default=0.001, help='Taxa de aprendizado do discriminador')
     parser.add_argument('--g_lr', type=float, default=0.001, help='Taxa de aprendizado do gerador')
-    parser.add_argument('--epochs', type=int, default=50, help='Número de épocas de treinamento')
-    parser.add_argument('--save_every', type=int, default=50, help='Salvar pesos a cada número de épocas')
+    parser.add_argument('--epochs', type=int, default=100, help='Número de épocas de treinamento')
+    parser.add_argument('--save_every', type=int, default=100, help='Salvar pesos a cada número de épocas')
 
     args = parser.parse_args()
 
@@ -53,6 +54,8 @@ def main():
     g_lr = args.g_lr
     epochs = args.epochs
     save_every = args.save_every
+
+    model_fname = f"{H:04d}{W:04d}{width:03d}{depth:02d}{batch_size:03d}{int(d_lr*1e5):05d}{int(g_lr*1e5):05d}"
 
     train_set = image_dataset_from_directory(
         path_train, labels=None, batch_size=batch_size, image_size=(H, W), shuffle=True
@@ -75,7 +78,8 @@ def main():
         distortion_loss_fn=losses.MeanSquaredError()
     )
 
-    train(gan, train_set, val_set, epochs, save_every)
+    d_losses_train, g_losses_train, d_losses_val, g_losses_val = train(gan, train_set, val_set, epochs, save_every, model_fname)
+    np.savetxt(f"../models/{model_fname}_losses.txt", np.array([d_losses_train, g_losses_train, d_losses_val, g_losses_val]))
 
 if __name__ == '__main__':
     main()
