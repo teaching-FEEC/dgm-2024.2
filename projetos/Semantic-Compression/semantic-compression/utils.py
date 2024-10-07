88220e53bf03be935a507a434c55666361beb9f9
dioph <dioph@protonmail.com>
2024-10-07 10:46:30 -0300

    update cityscapes mean/var

--- projetos/Semantic-Compression/semantic-compression/utils.py
+++ projetos/Semantic-Compression/semantic-compression/utils.py
@@ -1,5 +1,3 @@
-# store useful variables and configuration
-
 from PIL import Image
 
 import tensorflow as tf
@@ -8,13 +6,15 @@ def webp2bmp(img):
     pass
 
 def normalize_input(x):
-    _mean = tf.convert_to_tensor([115., 114., 112.])
-    _var = tf.convert_to_tensor([1450., 1510., 1700.])
+    # valores baseados no conjunto de treino do cityscapes
+    _mean = tf.convert_to_tensor([72., 82., 72.])
+    _var = tf.convert_to_tensor([2060., 2160., 2110.])
     return (x - _mean) / tf.math.sqrt(_var + 1e-10)
 
-def denormalize_output(x0_hat):
-    _mean = tf.convert_to_tensor([115., 114., 112.])
-    _var = tf.convert_to_tensor([1450., 1510., 1700.])
-    x_hat = x0_hat * tf.math.sqrt(_var + 1e-10) + _mean
-    x_hat = tf.clip_by_value(x_hat, clip_value_min=0, clip_value_max=255)
-    return x_hat
+def denormalize_output(x_hat):
+    # valores baseados no conjunto de treino do cityscapes
+    _mean = tf.convert_to_tensor([72., 82., 72.])
+    _var = tf.convert_to_tensor([2060., 2160., 2110.])
+    x = x_hat * tf.math.sqrt(_var + 1e-10) + _mean
+    x = tf.clip_by_value(x, clip_value_min=0, clip_value_max=255)
+    return x
