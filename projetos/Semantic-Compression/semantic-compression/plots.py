81a8e2ed750a9f99ab9bd79750f11ddb97491e86
dioph <dioph@protonmail.com>
2024-10-08 01:08:43 -0300

    fix uint8

--- projetos/Semantic-Compression/semantic-compression/plots.py
+++ projetos/Semantic-Compression/semantic-compression/plots.py
@@ -7,7 +7,7 @@ def make_plot(model, losses, test_batch):
 
     gs = fig.add_gridspec(2, 4, width_ratios=[2, 2, 2, 2]) 
     ax1 = fig.add_subplot(gs[0, 0:2]) 
-    ax1.set_ylim(-1, 2)
+    # ax1.set_ylim(-1, 2)
     ax1.plot(losses[0], label='Loss_train')
     ax1.plot(losses[2], label='Loss_val')
     ax1.set_title('D_losses')
@@ -16,7 +16,7 @@ def make_plot(model, losses, test_batch):
     ax1.legend()
 
     ax2 = fig.add_subplot(gs[1, 0:2])  
-    ax2.set_ylim(0, 12)
+    # ax2.set_ylim(0, 12)
     ax2.plot(losses[1], label='Loss_train')
     ax2.plot(losses[3], label='Loss_val')
     ax2.set_title('G_losses')
@@ -25,7 +25,7 @@ def make_plot(model, losses, test_batch):
     ax2.legend()
 
     ax_img1 = fig.add_subplot(gs[0, 2:4]) 
-    ax_img1.imshow(img1)
+    ax_img1.imshow(np.uint8(test_batch[0]))
     ax_img1.axis('off')  
 
     ax_img2 = fig.add_subplot(gs[1, 2:4])  
