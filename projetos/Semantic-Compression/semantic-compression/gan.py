d6edd2e7b4661b896e8f982fa40bdb08eba28051
dioph <dioph@protonmail.com>
2024-10-20 20:48:36 -0300

    uma unica loss com reverse grad

--- projetos/Semantic-Compression/semantic-compression/gan.py
+++ projetos/Semantic-Compression/semantic-compression/gan.py
@@ -1,8 +1,8 @@
 import numpy as np
 import tensorflow as tf
-from tqdm.auto import tqdm
-from keras import layers, models, optimizers
-from utils import normalize_input, denormalize_output
+from keras import layers, models
+# from utils import normalize_input, denormalize_output
+
 
 class Block(layers.Layer):
     def __init__(self, filters, kernel_size):
@@ -26,6 +26,7 @@ class Block(layers.Layer):
         out = h2 + x_0
         return out
 
+
 class ResidualBlock(layers.Layer):
     def __init__(self, filters, kernel_size):
         super().__init__()
@@ -40,231 +41,254 @@ class ResidualBlock(layers.Layer):
         out = x + x_0
         return out
 
+
 class Encoder(models.Model):
-    def __init__(self, C, filters, n_blocks):
+    """
+    input: k7s1 [filters / 2**n_convs]
+    downsample: k5s2 [filters / 2**(n_convs-i)], i=1,2,...,n_convs
+    output: k3s1 [channels]
+    """
+    def __init__(self, channels, filters, n_convs=4):
         super().__init__()
-        self.C = C
+        self.channels = channels
         self.filters = filters
-        self.n_blocks = n_blocks
-        self.res_blocks = [ResidualBlock(self.filters, 3) for _ in range(self.n_blocks)]
-        self.h1 = models.Sequential([
-            layers.Conv2D(self.filters // 2, kernel_size=5, strides=2, padding='same'),
+        self.n_convs = n_convs
+        self.h_in = models.Sequential([
+            layers.Conv2D(
+                self.filters // 2**self.n_convs,
+                kernel_size=7, strides=1, padding='same'
+            ),
             layers.BatchNormalization(),
             layers.ReLU()
         ])
-        self.h2 = models.Sequential([
-            layers.Conv2D(self.filters, kernel_size=5, strides=2, padding='same'),
-            layers.BatchNormalization(),
-            layers.ReLU()
-        ])
-        self.block = Block(self.filters, 3)
-        self.h3 = models.Sequential([
-            layers.Conv2D(self.C, kernel_size=5, strides=2, padding='same'),
+        self.downsample = models.Sequential()
+        for i in range(self.n_convs):
+            self.downsample.add(
+                layers.Conv2D(
+                    self.filters // 2**(self.n_convs - i - 1),
+                    kernel_size=5, strides=2, padding='same'
+                )
+            )
+            self.downsample.add(layers.BatchNormalization())
+            self.downsample.add(layers.ReLU())
+        self.h_out = models.Sequential([
+            layers.Conv2D(self.channels, kernel_size=3, strides=1, padding='same'),
             layers.BatchNormalization()
         ])
 
     def call(self, inputs, training=False):
-        h1 = self.h1(inputs, training=training)
-        x = self.h2(h1, training=training)
-        x_0 = x
-        for i in range(self.n_blocks):
-            x = self.res_blocks[i](x, training=training)
-        x = self.block(x, training=training)
-        w = x + x_0
-        w = self.h3(w, training=training)
+        h_in = self.h_in(inputs, training=training)
+        h_d = self.downsample(h_in, training=training)
+        w = self.h_out(h_d, training=training)
         return w
 
 
+class Quantizer(layers.Layer):
+    def __init__(self, sigma, levels, c_min, c_max):
+        super().__init__()
+        self.sigma = sigma
+        self.levels = levels
+        self.c_min = c_min
+        self.c_max = c_max
+
+    def call(self, z):
+        # convert z -> (B, C, H, W)
+        z_t = tf.transpose(z, (0, 3, 1, 2))
+        z_t_shape = tf.shape(z_t)
+        B = z_t_shape[0]
+        C = z_t_shape[1]
+        z_vec = tf.reshape(z_t, [B, C, -1, 1])
+        # definir array centers
+        centers = tf.linspace(float(-2), float(2), 5)
+        # definir tensor dist
+        dist = tf.square(tf.abs(z_vec - centers))
+        # calcular tensor z_hat
+        phi = tf.nn.softmax(-self.sigma * dist, axis=-1)
+        z_hat = tf.reshape(tf.reduce_sum(phi * centers, axis=3), z_t_shape)
+        z_hat = tf.transpose(z_hat, (0, 2, 3, 1))
+        return z_hat
+
+
 class Generator(models.Model):
-    def __init__(self, filters, n_blocks):
+    """
+    input: k3s1 [filters]
+    resnet: n_blocks x (k3s1 [filters] + k3s1 [filters])
+    upsample: k5s2 [filters / 2**i], i=1,2,...,n_convs
+    output: k7s1 [3]
+    """
+    def __init__(self, n_blocks, filters, n_convs=4):
         super().__init__()
         self.filters = filters
         self.n_blocks = n_blocks
-        self.res_blocks = [ResidualBlock(self.filters, 3) for _ in range(self.n_blocks)]
-        self.h1 = models.Sequential([
-            layers.Conv2DTranspose(self.filters, kernel_size=3, strides=2, padding='same'),
+        self.res_blocks = [ResidualBlock(self.filters, kernel_size=3) for _ in range(self.n_blocks)]
+        self.h_in = models.Sequential([
+            layers.Conv2DTranspose(self.filters, kernel_size=3, strides=1, padding='same'),
             layers.BatchNormalization(),
             layers.ReLU()
         ])
-        self.block = Block(self.filters, 3)
-        self.h2 = models.Sequential([
-            layers.Conv2DTranspose(self.filters // 2, kernel_size=5, strides=2, padding='same'),
+        self.upsample = models.Sequential()
+        for i in range(n_convs):
+            self.upsample.add(
+                layers.Conv2DTranspose(
+                    self.filters // 2**(i + 1),
+                    kernel_size=5, strides=2, padding='same'
+                )
+            )
+            self.upsample.add(layers.BatchNormalization())
+            self.upsample.add(layers.ReLU())
+        self.h_out = models.Sequential([
+            layers.Conv2DTranspose(3, kernel_size=7, strides=1, padding='same'),
             layers.BatchNormalization(),
-            layers.ReLU()
-        ])
-        self.h3 = models.Sequential([
-            layers.Conv2DTranspose(3, kernel_size=5, strides=2, padding='same'),
-            layers.BatchNormalization()
         ])
 
     def call(self, inputs, training=False):
-        w_hat = self.h1(inputs, training=training)
-        w_0 = w_hat
+        h = self.h_in(inputs, training=training)
         for i in range(self.n_blocks):
-            w_hat = self.res_blocks[i](w_hat, training=training)
-        w_hat = self.block(w_hat, training=training)
-        x_hat = w_hat + w_0
-        x_hat = self.h2(x_hat, training=training)
-        x_hat = self.h3(x_hat, training=training)
+            h = self.res_blocks[i](h, training=training)
+        h_up = self.upsample(h, training=training)
+        x_hat = self.h_out(h_up, training=training)
+        return x_hat
+
+
+class AutoEncoder(models.Model):
+    def __init__(self, filters, n_blocks, channels, n_convs, sigma, levels, c_min, c_max):
+        super().__init__()
+        self.encoder = Encoder(channels, filters, n_convs)
+        self.decoder = Generator(n_blocks, filters, n_convs)
+        self.quantizer = Quantizer(sigma, levels, c_min, c_max)
+
+    def call(self, x, training=False):
+        w = self.encoder(x, training=training)
+        w_hat = self.quantizer(w)
+        x_hat = self.decoder(w_hat, training=training)
         return x_hat
 
 
 class Discriminator(models.Model):
-    def __init__(self, filters):
+    """
+    downsample: k4s2 [filters / 2**(n_convs-i)], i=1,2,...,n_convs
+    output: k4s1 [1] + dense(sigmoid)
+    """
+    def __init__(self, filters, n_convs=4):
         super().__init__()
         self.filters = filters
-        self.net = models.Sequential([
-            layers.Conv2D(self.filters, kernel_size=4, strides=2, padding='same', activation='relu'),
-            layers.BatchNormalization(),
-            layers.Conv2D(self.filters // 2, kernel_size=4, strides=2, padding='same', activation='relu'),
-            layers.BatchNormalization(),
-            layers.Conv2D(self.filters // 4, kernel_size=4, strides=2, padding='same', activation='relu'),
-            layers.BatchNormalization(),
+        self.n_convs = n_convs
+        self.net = models.Sequential()
+        for i in range(self.n_convs):
+            self.net.add(
+                layers.Conv2D(
+                    self.filters // 2**(self.n_convs - i - 1),
+                    kernel_size=4, strides=2, padding='same'
+                )
+            )
+            self.net.add(layers.BatchNormalization())
+            self.net.add(layers.LeakyReLU(0.2))
+        self.h_out = models.Sequential([
+            layers.Conv2D(1, kernel_size=4, strides=1, padding='same'),
             layers.Flatten(),
             layers.Dense(1, activation='sigmoid')
         ])
 
     def call(self, inputs, training=False):
-        out = self.net(inputs, training=training)
-        return out
+        features = self.net(inputs, training=training)
+        y = self.h_out(features, training=training)
+        return y
 
 
-def quantizer(L, c_min, c_max):
-    """
-    z: ndarray(B, H, W, C)
-    L: int
-    """
-    def q(z):
-        # convert z -> (B, C, H, W)
-        z_t = tf.transpose(z, (0, 3, 1, 2))
-        z_t_shape = tf.shape(z_t)
-        B, C = z_t_shape[:2]
-        z_vec = tf.reshape(z_t, [B, C, -1, 1])
-        # definir array centers
-        centers = np.linspace(float(c_min), float(c_max), L)
-        # definir tensor dist
-        dist = tf.square(tf.abs(z_vec - centers))
-        # calcular tensor z_hat
-        symbols = tf.reshape(tf.argmin(dist, axis=-1), z_t_shape)
-        symbols = tf.transpose(symbols, (0, 2, 3, 1))
-        z_hat = tf.convert_to_tensor(centers[symbols])
-        return z_hat, symbols
-    return q
-
-
-class AutoEncoder(models.Model):
-    def __init__(self, L, e_filters, e_blocks, g_filters, g_blocks, c_min, c_max):
+class NegativeGradientLayer(layers.Layer):
+    def __init__(self):
         super().__init__()
-        self.encoder = Encoder(L, e_filters, e_blocks)
-        self.decoder = Generator(g_filters, g_blocks)
-        self.quantizer = quantizer(L, c_min, c_max)
 
-    def call(self, x, training=False):
-        w = self.encoder(x, training=training)
-        w_hat, _ = self.quantizer(w)
-        x_hat = self.decoder(w_hat, training=training)
-        return x_hat, w_hat
+    def call(self, inputs):
+        @tf.custom_gradient
+        def identity_with_neg_grad(x):
+            def grad(dy):
+                # Return the negative of the incoming gradient
+                return -dy
+            return x, grad
+        # Apply the custom gradient operation to the inputs
+        return identity_with_neg_grad(inputs)
 
 
 class GenerativeCompressionGAN(models.Model):
-    def __init__(self, e_filters, e_blocks, g_filters, g_blocks, d_filters, *, L=5, c_min=-2, c_max=2, lambda_d=10, **kwargs):
+    def __init__(self, filters, n_blocks, channels, n_convs, *, sigma=1000, levels=5, c_min=-2, c_max=2, lambda_d=10, **kwargs):
         super().__init__(**kwargs)
-        self.e_filters = e_filters
-        self.e_blocks = e_blocks
-        self.g_filters = g_filters
-        self.g_blocks = g_blocks
-        self.d_filters = d_filters
+        self.filters = filters
+        self.n_blocks = n_blocks
+        self.channels = channels
+        self.n_convs = n_convs
+        self.sigma = sigma
+        self.levels = levels
         self.c_min = c_min
         self.c_max = c_max
-        self.autoencoder = AutoEncoder(L, e_filters, e_blocks, g_filters, g_blocks, c_min, c_max)
-        self.discriminator = Discriminator(d_filters)
-        self.quantizer = quantizer(L=L, c_min=c_min, c_max=c_max)
-        self.lambda_d = lambda_d  # Peso para a perda de distorção
-        self.L = L  # Número de níveis de quantização
+        self.lambda_d = lambda_d
+        self.preprocess = layers.Rescaling(1./255.)
+        self.autoencoder = AutoEncoder(filters, n_blocks, channels, n_convs, sigma, levels, c_min, c_max)
+        self.reverse_layer = NegativeGradientLayer()
+        self.discriminator = Discriminator(filters, n_convs)
+        self.loss_fn = least_squares_gan_loss
 
-    def compile(self, d_optimizer, g_optimizer, gan_loss_fn, distortion_loss_fn):
-        super().compile()
-        self.d_optimizer = d_optimizer
-        self.g_optimizer = g_optimizer
-        self.gan_loss_fn = gan_loss_fn  # Perda adversarial
-        self.distortion_loss_fn = distortion_loss_fn  # Perda de distorção
+    def compile(self, optimizer_1, optimizer_2, **kwargs):
+        super().compile(**kwargs)
+        self.opt1 = optimizer_1
+        self.opt2 = optimizer_2
 
-    def call(self, data):
-        real_images = normalize_input(data)
-        generated_images, quantized_latent = self.autoencoder(real_images)
-        return denormalize_output(generated_images), quantized_latent
+    def call(self, x, training=False):
+        x0 = self.preprocess(x)
+        x_hat = self.autoencoder(x0, training=training)
+        x_tilde = self.reverse_layer(x_hat)
+        y = self.discriminator(x, training=training)
+        y_hat = self.discriminator(x_tilde)
+        return x0, x_hat, y, y_hat
 
-    def evaluate(self, dataloader_val):
-        d_loss_val = 0.
-        g_loss_val = 0.
-        for data in tqdm(dataloader_val):
-            real_images = normalize_input(data)
-            generated_images, quantized_latent = self.autoencoder(real_images)
-            real_output = self.discriminator(real_images)
-            fake_output = self.discriminator(generated_images)
-            # Perda adversarial do discriminador
-            d_loss_real = self.gan_loss_fn(tf.ones_like(real_output), real_output)
-            d_loss_fake = self.gan_loss_fn(tf.zeros_like(fake_output), fake_output)
-            d_loss_batch = (d_loss_real + d_loss_fake) / 2
-            d_loss_val += d_loss_batch
-            # Perda adversarial para o gerador (Least-Squares GAN)
-            g_gan_loss = self.gan_loss_fn(tf.ones_like(fake_output), fake_output)
-            # Perda de distorção
-            g_distortion_loss = self.distortion_loss_fn(real_images, generated_images)
-            # Perda total ponderada
-            g_loss_batch = g_gan_loss + self.lambda_d * g_distortion_loss
-            g_loss_val+= g_loss_batch
-        d_loss_avg = d_loss_val / len(dataloader_val)
-        g_loss_avg = g_loss_val / len(dataloader_val)
-        return d_loss_avg, g_loss_avg
+    def train_step(self, x, discriminating=True):
+        with tf.GradientTape() as tape:
+            x0, x_hat, y, y_hat = self(x, training=True)
+            gan_loss, l2_loss = self.loss_fn(x0, x_hat, y, y_hat)
+            if discriminating:
+                loss = self.lambda_d * l2_loss - gan_loss
+                train_vars = self.trainable_variables
+                opt = self.opt2
+            else:
+                loss = l2_loss
+                train_vars = self.autoencoder.trainable_variables
+                opt = self.opt1
+            grads = tape.gradient(loss, train_vars)
+            opt.apply_gradients(zip(grads, train_vars))
+        return gan_loss, l2_loss
 
-    def train_step(self, dataloader_train):
-        d_loss = 0.
-        g_loss = 0.
-        for data in tqdm(dataloader_train):
-            real_images = normalize_input(data)
-            # Train the discriminator (max GAN loss)
-            with tf.GradientTape() as tape:
-                generated_images, quantized_latent = self.autoencoder(real_images)
-                real_output = self.discriminator(real_images)
-                fake_output = self.discriminator(generated_images)
-                # Perda adversarial do discriminador
-                d_loss_real = self.gan_loss_fn(tf.ones_like(real_output), real_output)
-                d_loss_fake = self.gan_loss_fn(tf.zeros_like(fake_output), fake_output)
-                d_loss_batch = (d_loss_real + d_loss_fake) / 2
-                d_loss += d_loss_batch
-                grads = tape.gradient(d_loss_batch, self.discriminator.trainable_weights)
-                self.d_optimizer.apply_gradients(zip(grads, self.discriminator.trainable_weights))
-            # Train the autoencoder (min GAN loss + distortion loss)
-            with tf.GradientTape() as tape:
-                generated_images, quantized_latent = self.autoencoder(real_images)
-                fake_output = self.discriminator(generated_images)
-                # Perda adversarial para o gerador (Least-Squares GAN)
-                g_gan_loss = self.gan_loss_fn(tf.ones_like(fake_output), fake_output)
-                # Perda de distorção
-                g_distortion_loss = self.distortion_loss_fn(real_images, generated_images)
-                # Perda total ponderada
-                g_loss_batch = g_gan_loss + self.lambda_d * g_distortion_loss
-                g_loss += g_loss_batch
-                grads = tape.gradient(g_loss_batch, self.autoencoder.trainable_weights)
-                self.g_optimizer.apply_gradients(zip(grads, self.autoencoder.trainable_weights))
-        d_loss_avg = d_loss / len(dataloader_train)
-        g_loss_avg = g_loss / len(dataloader_train)
-        return d_loss_avg, g_loss_avg
+    def test_step(self, x):
+        x0, x_hat, y, y_hat = self(x)
+        return self.loss_fn(x0, x_hat, y, y_hat)
 
     def get_config(self):
         config = super().get_config()
         config.update(
             {
-                "e_filters": self.e_filters,
-                "e_blocks": self.e_blocks,
-                "g_filters": self.g_filters,
-                "g_blocks": self.g_blocks,
-                "d_filters": self.d_filters,
-                "L": self.L,
+                "filters": self.filters,
+                "n_blocks": self.n_blocks,
+                "channels": self.channels,
+                "n_convs": self.n_convs,
+                "sigma": self.sigma,
+                "levels": self.levels,
                 "c_min": self.c_min,
                 "c_max": self.c_max,
                 "lambda_d": self.lambda_d,
             }
         )
         return config
+
+    @classmethod
+    def from_config(cls, config):
+        return cls(
+            config['filters'], config['n_blocks'], config['channels'], config['n_convs'],
+            sigma=config['sigma'], levels=config['levels'],
+            c_min=config['c_min'], c_max=config['c_max'],
+            lambda_d=config['lambda_d']
+        )
+
+
+def least_squares_gan_loss(x, x_hat, y, y_hat):
+    gan_loss = tf.reduce_mean(y**2) + tf.reduce_mean((y_hat - 1)**2)
+    l2_loss =  tf.reduce_mean((x - x_hat)**2)
+    return gan_loss, l2_loss
