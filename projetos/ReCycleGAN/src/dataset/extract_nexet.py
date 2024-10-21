b6d03a5b9c6b79fa0cc0848b8c4c097c2ce66cab
Tiago Amorim <tiago.ca.amorim@gmail.com>
2024-10-21 14:51:16 -0300

    Corrected extract_nexet script

--- projetos/ReCycleGAN/src/dataset/extract_nexet.py
+++ projetos/ReCycleGAN/src/dataset/extract_nexet.py
@@ -1,43 +1,39 @@
-# # Extract DayNight Images
-# 
-# Create DayNight database for model training.
+# pylint: disable=C0413
+"""Create DayNight database for model training."""
 
 import sys
 from pathlib import Path
 import pandas as pd
+import matplotlib.pyplot as plt
 
-current_dir = Path().resolve()
-sys.path.append(str(current_dir.parent))
-import utils
+sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
+from src.utils import ImageTools, utils
 
-labels_file_name = '../../data/external/nexet/nexet/train.csv'
+df_labels = pd.read_csv('projetos/ReCycleGAN/no_sync/nexet/nexet/train.csv')
+img_folder = Path('projetos/ReCycleGAN/no_sync/nexet/nexet/nexet_2017_1/')
+out_folder = Path('projetos/ReCycleGAN/data/external/nexet/')
 
-df_labels = pd.read_csv(labels_file_name)
-
-img_folder = '../../data/external/nexet/nexet/nexet_2017_1/'
-
-img_folder = Path(img_folder)
 file_count = len([f for f in img_folder.iterdir() if f.is_file()])
 print(f'There are {file_count} files in the folder.')
 
-df_count = utils.img_size_count(img_folder)
-utils.img_size_count_plot(df_count, show=True)
+df_count = ImageTools.img_size_count(img_folder)
+ImageTools.img_size_count_plot(df_count)
+out_folder.mkdir(parents=True, exist_ok=True)
+plt.savefig(out_folder / 'img_size_count.png')
 
 folders = {
         'input_A': {'lighting':['Day'], 'city':['NYC']},
         'input_B': {'lighting':['Night'], 'city':['NYC']},
     }
 
-for out_folder, df_filter in folders.items():
+for folder, df_filter in folders.items():
     img_list = utils.filter_dataframe(df_labels, df_filter)['image_filename']
-    utils.Images.build_dataset(
+    ImageTools.build_dataset(
         img_list=img_list,
         img_folder=Path(img_folder),
-        output_folder=Path('../../data/external/nexet/') / out_folder,
-        transformation_function=utils.resize_and_crop,
+        output_folder=out_folder / folder,
+        transformation_function=ImageTools.resize_and_crop,
         transformation_params={'target_size':(256,256), 'size_filter':[(1280, 720)]},
         split=0.8,
         random_seed=42
     )
-
-
