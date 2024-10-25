a058b29c7a8b6f5928836b4188ef4a7c6c0916f3
TiagoCAAmorim <tiago.ca.amorim@gmail.com>
2024-10-25 14:40:25 -0300

    download.py run by itself copies NEXET files to usual folder

--- projetos/ReCycleGAN/src/dataset/download.py
+++ projetos/ReCycleGAN/src/dataset/download.py
@@ -6,25 +6,38 @@ import requests
 # URL of the ZIP file
 NEXET = 'https://github.com/TiagoCAAmorim/dgm-2024.2/releases/download/v0.1.0-nexet/Nexet.zip'
 
-def download_and_extract(url, path):
+def download_and_extract(url, path, verbose=False):
     """
     Download and extract a ZIP file from a URL.
 
     Args:
     - url: URL of the ZIP file.
     - path: Path to extract the ZIP file.
+    - verbose: Bool to print messages. Default is False.
     """
     path = Path(path)
     zip_path = path / 'file.zip'
+    if verbose:
+        print(f'Creating folder: {zip_path.parent}')
     zip_path.parent.mkdir(parents=True, exist_ok=True)
     response = requests.get(url, timeout=10)
+    if verbose:
+        print(f'Downloading file: {zip_path.name}')
     with open(zip_path, 'wb') as f:
         f.write(response.content)
 
+    if verbose:
+        print('Unpaking zip file.')
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         zip_ref.extractall(path)
 
+    if verbose:
+        print('Deleting zip file.')
     zip_path.unlink()
+    if verbose:
+        print('Done.')
 
 if __name__ == '__main__':
-    download_and_extract(NEXET, Path(__file__).parent)
+    # Downloads and extracts to 'usual' folder.
+    out_folder = Path(__file__).parent.parent.parent / 'data/external/nexet'
+    download_and_extract(NEXET, out_folder, verbose=True)
