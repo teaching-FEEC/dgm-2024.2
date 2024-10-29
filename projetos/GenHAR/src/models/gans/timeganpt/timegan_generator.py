c585109742e2453769c5c9781f2238e18377396f
Bruno Silva <bruno_gsilva@outlook.com>
2024-10-29 10:18:58 -0300

    Added TSGM TimeGAN implementation

--- projetos/GenHAR/src/models/gans/timeganpt/timegan_generator.py
+++ projetos/GenHAR/src/models/gans/timeganpt/timegan_generator.py
@@ -4,40 +4,22 @@ from utils.dataset_utils import split_axis_reshape, dict_class_samples
 
 # from models.gans.timeganpt.utils import sine_data_generation,MinMaxScaler
 # from sklearn.preprocessing import StandardScaler
-import numpy as np
 import torch
-import torch.nn as nn
-from torch import optim
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-
-from sklearn.manifold import TSNE
-from sklearn.decomposition import PCA
-import matplotlib.pyplot as plt
 
 from models.gans.timeganpt.utils import extract_time
 from models.gans.timeganpt.utils import random_generator
 from models.gans.timeganpt.utils import MinMaxScaler
 from models.gans.timeganpt.utils import MinMaxUnscaler
-from models.gans.timeganpt.utils import sine_data_generation
-from models.gans.timeganpt.utils import visualization
 
 from models.gans.timeganpt.modules_and_training import TimeGAN
-from models.gans.timeganpt.modules_and_training import Time_GAN_module
+import tsgm
 import pandas as pd
 
 
 class TimeGANGenerator:
     def __init__(self, m_config):
         self.m_config = m_config
-        self.models = {}  # Dicionário para armazenar modelos TimeGAN por classe
-        parameters = dict()
-        parameters["module"] = "gru"
-        parameters["hidden_dim"] = 24
-        parameters["num_layers"] = 3
-        parameters["iterations"] = 100
-        parameters["batch_size"] = 128
-        parameters["epoch"] = 500
+        self.use_tsgm = m_config["use_tsgm"] if "use_tsgm" in m_config.keys() else False
         self.parameters = self.m_config["parameters"]
 
     def reshape_data(self, x_df, y_train):
@@ -73,6 +55,12 @@ class TimeGANGenerator:
         return test_recovery
 
     def train(self, X_train, y_train):
+        if self.use_tsgm:
+            self.train_tsgm(X_train, y_train)
+        else:
+            self.train_pt_impl(X_train, y_train)
+
+    def train_pt_impl(self, X_train, y_train):
         # output_size = 20
         gamma = 1
         # no, seq_len, dim = 12800, 24, 5
@@ -125,7 +113,39 @@ class TimeGANGenerator:
             self.checkpoints[class_label] = checkpoints
             self.data[class_label] = data
 
+    def train_tsgm(self, X_train, y_train):
+
+        self.model = {}
+        self.scaler = {}
+
+        x_df = X_train.copy()
+        self.columns_names = x_df.columns
+
+        x_data = split_axis_reshape(x_df)
+        class_data_dict = dict_class_samples(x_data, y_train.copy())
+        for class_label in class_data_dict.keys():
+            train_data = class_data_dict[class_label]
+            transform_data = train_data.transpose(0, 2, 1).reshape(-1, 6)
+            transform_data, min, max = MinMaxScaler(transform_data)
+            self.scaler[class_label] = {"min": min, "max": max}
+            data = transform_data.reshape(train_data.shape[0], 60, 6)  # .transpose(0, 2, 1)
+            self.model[class_label] = tsgm.models.timeGAN.TimeGAN(
+                seq_len=60,
+                module=self.parameters["module"],
+                hidden_dim=self.parameters["hidden_dim"],
+                n_features=6,
+                n_layers=self.parameters["num_layers"],
+                batch_size=self.parameters["batch_size"],
+            )
+            self.model[class_label].fit(data, self.parameters["epoch"])
+
     def generate(self, num_samples_per_class):
+        if self.use_tsgm:
+            return self.generate_tsgm(num_samples_per_class)
+        else:
+            return self.generate_pt_impl(num_samples_per_class)
+
+    def generate_pt_impl(self, num_samples_per_class):
         synthetic_df = pd.DataFrame()
 
         # Loop para gerar dados sintéticos por classe
@@ -157,12 +177,37 @@ class TimeGANGenerator:
 
         return synthetic_df
 
+    def generate_tsgm(self, num_samples_per_class):
+        synthetic_df = pd.DataFrame()
+
+        # Loop para gerar dados sintéticos por classe
+        for class_label in self.model.keys():
+            # Obter o modelo do gerador correspondente
+            synthetic_data = self.model[class_label].generate(num_samples_per_class)
+            synthetic_data = synthetic_data.reshape(-1, 6)
+            synthetic_data = MinMaxUnscaler(synthetic_data, self.scaler[class_label])
+            synthetic_data = synthetic_data.reshape(-1, 60, 6)
+            print(type(synthetic_data))
+            synthetic_data = synthetic_data.transpose(0, 2, 1)
+            synthetic_data = synthetic_data.reshape(synthetic_data.shape[0], -1)
+
+            # Criar um DataFrame para os dados sintéticos da classe atual
+            class_df = pd.DataFrame(synthetic_data)
+            class_df.columns = self.columns_names
+            # Adicionar uma coluna para a classe
+            class_df["label"] = [class_label] * synthetic_data.shape[0]
+
+            # Concatenar com o DataFrame geral
+            synthetic_df = pd.concat([synthetic_df, class_df], ignore_index=True)
+
+        return synthetic_df
+
     def save_model(self, file_path):
         # Salvar o dicionário de modelos treinados como um arquivo .pkl
         with open(file_path, "wb") as f:
-            pickle.dump(self.models, f)
+            pickle.dump(self.model, f)
 
     def load_model(self, file_path):
         # Carregar o dicionário de modelos a partir de um arquivo .pkl
         with open(file_path, "rb") as f:
-            self.models = pickle.load(f)
+            self.model = pickle.load(f)
