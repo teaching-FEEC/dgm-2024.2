41c76365641e5a472ab4b892c3bb92cefcf754fe
dioph <dioph@protonmail.com>
2024-11-17 12:02:02 -0300

    load dataset beforehand

--- projetos/Semantic-Compression/semantic-compression/dataset.py
+++ projetos/Semantic-Compression/semantic-compression/dataset.py
@@ -7,7 +7,9 @@ import numpy as np
 from cityscapesscripts.download.downloader import download_packages, login
 from PIL import Image
 from scipy.io import loadmat
+from torch import nn
 from torch.utils import data
+from torchvision.transforms import v2
 from tqdm.auto import tqdm
 
 
@@ -22,24 +24,20 @@ class SemanticResize(nn.Module):
 
 
 class SemanticData(data.Dataset):
-    def __init__(self, folder_path, transform=None, x_transform=None, s_transform=None):
+    def __init__(self, folder_path, crop_shape, transform=None, x_transform=None, s_transform=None):
         super().__init__()
         self.transform = transform
         self.x_transform = x_transform
         self.s_transform = s_transform
-        self.img_files = glob.glob(os.path.join(folder_path, 'images', '*.jpg'))
-        self.mask_files = []
-        for img_path in self.img_files:
-            fn = os.path.basename(img_path)
-            fn, _ = os.path.splitext(fn)
-            self.mask_files.append(os.path.join(folder_path, 'semantic', fn + '.png')) 
+        self.img_files = glob.glob(os.path.join(folder_path, 'images', '*.png'))
+        self.img_files += glob.glob(os.path.join(folder_path, 'images', '*.jpg'))
+        fns = [os.path.splitext(os.path.basename(img_path))[0] for img_path in self.img_files]
+        self.map_files = [os.path.join(folder_path, 'maps', fn + '.png') for fn in fns]
+        self.data = [v2.CenterCrop(size=crop_shape)(Image.open(img_path).convert('RGB')) for img_path in self.img_files]
+        self.labels = [v2.CenterCrop(size=crop_shape)(Image.open(map_path)) for map_path in self.map_files]
 
     def __getitem__(self, index):
-            img_path = self.img_files[index]
-            mask_path = self.mask_files[index]
-            data = Image.open(img_path)
-            label = Image.open(mask_path)
-            sample = {'x': data, 's': label}
+            sample = {'x': self.data[index], 's': self.labels[index]}
             if self.transform:
                 sample = self.transform(sample)
             if self.x_transform:
@@ -57,10 +55,10 @@ def download_cityscapes():
     download_packages(session=session, package_names=["leftImg8bit_trainvaltest.zip", "gtFine_trainvaltest.zip"], destination_path="data/", resume=True)
 
 def organize_split(root, fnames):
-    mask_path = "data/coco/annotations"
+    map_path = "data/coco/annotations"
     img_path = "data/coco/images"
     for i,fname in tqdm(enumerate(fnames)):
-        s = loadmat(os.path.join(mask_path, fname) + '.mat')['S']
+        s = loadmat(os.path.join(map_path, fname) + '.mat')['S']
         fn = os.path.join(root, "semantic", fname)
         Image.fromarray(s).save(f"{fn}.png")
         shutil.move(os.path.join(img_path, fname) + '.jpg', os.path.join(root, "images"))
@@ -75,7 +73,6 @@ def process_cocostuff():
     with open("data/coco/imageLists/test.txt") as f:
         fnames = f.read().splitlines()
     organize_split(val_path, fnames)
-    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
